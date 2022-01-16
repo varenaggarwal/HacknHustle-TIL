71655cb547accb05b02980c1a840b841c5c4f44e
Varen Aggarwal <varen.aggarwal@timesinternet.in>
2022-01-16 15:29:03 +0530

    resolve merge conflict

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,8 @@
-import "./../css/modal.css";
-import React, { useEffect, useState } from "react";
-import { getDatafromServer } from "../serverRequest";
-import TweetContainer from "./TweetContainer";
+import './../css/modal.css';
+import React from 'react';
 
 const Modal = ({ handleClose, show, children }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
-  const [state, setState] = useState({});
-
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getDatafromServer();
-      setState(data.data[0]);
-    }
-    fetchData();
-  }, []);
 
   return (
     <div className={showHideClassName}>
@@ -21,20 +10,6 @@ const Modal = ({ handleClose, show, children }) => {
         {children}
         {/* <button className="close_modal" type="button" onClick={handleClose}>
           Close
-        </button>
-        {console.log(state)}
-        {state.hasOwnProperty("trends") &&
-          state.trends.map((item, index) => {
-            return (
-              <div key={index}>
-                <p>{item.category}</p>
-                <p>{item.name}</p>
-                <section>
-                </section>
-              </div>
-            );
-          })}
-          <TweetContainer id={"1482383366607765507"} />
         </button> */}
         <div className="close_modal close-icon"  onClick={handleClose}>
           <span><img src="close.png"></img></span>
